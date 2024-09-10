75d442de992d6d020da08a25660bbb2c0e81e10e
dekersab3u <thomas.de-kersabiec1@etu.univ-lorraine.fr>
2024-09-10 17:10:08 +0200

    ajout implementation servicerdv

--- toubeelib_squelette/app/src/core/services/rdv/ServiceRDV.php
+++ toubeelib_squelette/app/src/core/services/rdv/ServiceRDV.php
@@ -2,7 +2,25 @@
 
 namespace toubeelib\core\services\rdv;
 
+use PHPUnit\Framework\Exception;
+use toubeelib\core\dto\rdvDTO;
+use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
+
 class ServiceRDV implements RdvServiceInterface
 {
 
+    private RdvRepositoryInterface $rdvRep;
+
+    public function __construct(RdvRepositoryInterface $rdvRep) {
+        $this->rdvRep = $rdvRep;
+    }
+    public function consulterRDV(string $ID): rdvDTO
+    {
+        try {
+            $rdv = this->rdvRep->getRDV($ID);
+            return new rdvDTO($rdv);
+        } catch $e Exception::
+
+        return
+    }
 }
