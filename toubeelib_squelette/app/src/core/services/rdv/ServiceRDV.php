4f8ac6aabdbe7f65495499a77368991c74d4c561
dekersab3u <thomas.de-kersabiec1@etu.univ-lorraine.fr>
2024-09-17 11:25:35 +0200

    serviceRDV creerRDV

--- toubeelib_squelette/app/src/core/services/rdv/ServiceRDV.php
+++ toubeelib_squelette/app/src/core/services/rdv/ServiceRDV.php
@@ -3,12 +3,15 @@
 namespace toubeelib\core\services\rdv;
 
 use PHPUnit\Framework\Exception;
+use toubeelib\core\domain\entities\praticien\Praticien;
 use toubeelib\core\domain\entities\rdv\RendezVous;
 use toubeelib\core\dto\InputPraticienDTO;
 use toubeelib\core\dto\PraticienDTO;
 use toubeelib\core\dto\rdvDTO;
 use toubeelib\core\repositoryInterfaces\PraticienRepositoryInterface;
 use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
+use toubeelib\core\services\praticien\ServicePraticien;
+use toubeelib\core\services\praticien\ServicePraticienInvalidDataException;
 
 
 class ServiceRDV implements RdvServiceInterface
@@ -45,8 +48,24 @@ class ServiceRDV implements RdvServiceInterface
 
     public function creerRDV(string $ID_Patient, string $ID_Praticien, string $status, string $specialite, \DateTime $dateRdv): rdvDTO
     {
+        // Vérification de l'existence du praticien et de sa spécialité
+        try {
+            $prat = $this->praticienService->getPraticienById($ID_Praticien); // Appel de la méthode qui peut lever une exception
+
+            // Vérification de la spécialité
+            if ($specialite !== $prat->specialite) {
+                throw new ServiceRdvInvalidDataException("La spécialité spécifiée ne correspond pas au praticien indiqué");
+            }
+
+        } catch (ServicePraticienInvalidDataException $e) {
+            // Gestion de l'exception si le praticien n'existe pas
+            throw new ServiceRdvInvalidDataException("Praticien non valide : " . $e->getMessage());
+        }
+
+        // Création du rendez-vous après validation des données
         $rdv = new RendezVous($ID_Patient, $ID_Praticien, $status, $dateRdv);
         return $rdv->toDTO();
     }
+
 
 }
