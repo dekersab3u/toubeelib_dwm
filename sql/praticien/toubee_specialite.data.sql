5165d38e373ce7b5a6007805e2ff23440d203163
Clem's <115803908+KiSsWave@users.noreply.github.com>
2025-01-08 14:06:04 +0100

    simplification fichier sql

--- sql/praticien/toubee_specialite.data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "specialite" ("id", "label", "description") VALUES
-                                                            ('a85f5d5e-f97e-42b4-9ecf-6e0c676e1477', 'Dentiste', 'Spécialiste des dents'),
-                                                            ('b3e39c8d-0c58-4ff4-bd23-66d2f2043a2f', 'Ophtalmologue', 'Spécialiste des yeux'),
-                                                            ('c50bb74e-61d9-4f27-9375-6f85a23e5a85', 'Généraliste', 'Médecin généraliste'),
-                                                            ('d7f5f6f1-89c4-40fa-938e-8b48a6b1b7de', 'Pédiatre', 'Médecin pour enfants'),
-                                                            ('e86562e1-d77e-441a-93d0-5a2bf776abc1', 'Médecin du sport', 'Maladies et traumatismes liés à la pratique sportive');
