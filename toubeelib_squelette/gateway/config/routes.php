ea711a8f14debe6ad50e7914e010afeb12b8990e
Clem's <115803908+KiSsWave@users.noreply.github.com>
2025-01-15 16:02:01 +0100

    debut gateway

--- toubeelib_squelette/gateway/config/routes.php
+++ toubeelib_squelette/gateway/config/routes.php
@@ -1,1 +1,11 @@
 <?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+return function( \Slim\App $app):\Slim\App {
+
+    $app->get('/praticiens', \gateway\);
+    return $app;
+};
